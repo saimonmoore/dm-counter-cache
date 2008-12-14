1d9fcbca1d4b683e795743e81e0b87b7c4e7f84c
Saimon Moore <saimon@saimonmoore.net>
2008-12-14 19:00:27 +0100

    Check we actually have a parent model before doing any updating

--- lib/dm-counter-cache.rb
+++ lib/dm-counter-cache.rb
@@ -43,13 +43,13 @@ module DataMapper
             after :destroy, :decrement_counter_cache_for_#{name}
           
             def increment_counter_cache_for_#{name}
-              if self.class == #{model.name}
+              if self.#{name} && self.class == #{model.name}
                 self.#{name}.update_attributes(:#{counter_cache_attribute} => self.#{name}.reload.#{counter_cache_attribute}.succ)
               end
             end
 
             def decrement_counter_cache_for_#{name}
-              if self.class == #{model.name}
+              if self.#{name} && self.class == #{model.name}
                 self.#{name}.update_attributes(:#{counter_cache_attribute} => self.#{name}.reload.#{counter_cache_attribute} - 1)
               end
             end
