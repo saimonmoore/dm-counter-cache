e14db2f985bede50dcb616abdc8cc8ee31703417
Saimon Moore <saimon@saimonmoore.net>
2008-12-15 13:32:07 +0100

    We no longer try to dynamically assign counter cache property on parent model but rather do no inc/dec if not present. (i.e. counter properties must be explicitly assigned)

--- lib/dm-counter-cache.rb
+++ lib/dm-counter-cache.rb
@@ -32,23 +32,19 @@ module DataMapper
               counter_cache_attribute = "#{model.storage_name}_count".to_s
           end
 
-          unless relationship.parent_model.properties.has_property?(counter_cache_attribute.intern)
-            relationship.parent_model.class_eval <<-EOS, __FILE__, __LINE__
-              property :#{counter_cache_attribute}, Integer, :default => 0, :lazy => false          
-            EOS
-          end
-
           model.class_eval <<-EOS, __FILE__, __LINE__            
             after :create, :increment_counter_cache_for_#{name}
             after :destroy, :decrement_counter_cache_for_#{name}
           
             def increment_counter_cache_for_#{name}
+              return unless self.class.properties.has_property?(:#{counter_cache_attribute})
               if self.#{name} && self.class == #{model.name}
                 self.#{name}.update_attributes(:#{counter_cache_attribute} => self.#{name}.reload.#{counter_cache_attribute}.succ)
               end
             end
 
             def decrement_counter_cache_for_#{name}
+              return unless self.class.properties.has_property?(:#{counter_cache_attribute})
               if self.#{name} && self.class == #{model.name}
                 self.#{name}.update_attributes(:#{counter_cache_attribute} => self.#{name}.reload.#{counter_cache_attribute} - 1)
               end
